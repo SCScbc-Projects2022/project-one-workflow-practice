266fb9bca4b8e0c68e527c47f7a2a572882bb247
Candice Hall <hall.candice@outlook.com>
2022-04-01 13:47:11 -0400

    foundational html and js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var header = document.querySelector("#header");
+var containerOne = document.querySelector("#container-1");
+var containerTwo = document.querySelector("#container-2");
+var containerThree = document.querySelector("#container-3");
+// departure will be current location, destination will be search location
+
+
+
